a5b37ae884b95b1f72f414171b47aad75a151685
Leonio Santos <leoniopoint@gmail.com>
2021-04-03 09:56:49 -0300

    Aplicação de estudos ignite

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+//import styles from '../styles/home.module.scss'
+
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+    <title>Home | ig.news</title>
+  </Head>
+  <h1>Hello world</h1>
+    </>
+  )
+}
